fe0f33f51f5e1be12c4bc0a9be2fc410bde3ffee
Aarav Singh <aaravsingh@Aaravs-MacBook-Air.local>
2026-06-26 02:38:04 +0530

    updated bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,25 @@
-import discord
+import discord 
 from discord.ext import commands
 import config
 
-intents = discord.Intents.all()
-
+intents: discord.Intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f"Bot is ready. Logged in as {bot.user}")
+    await bot.tree.sync()
+    print("Bot is ready and slash commands synced.")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
+
+@bot.tree.command()
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("pong!")
+
+
+@bot.tree.command()
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hey!")
+
 
 bot.run(config.DISCORD_TOKEN)
